b2ca7f0f467b8a5c6a7c98a5966d832ca3c8cc3b
zahra <z.asadi1375@gmail.com>
2019-07-01 22:27:08 +0430

    template 16 created

--- README.md
+++
@@ -1,2 +0,0 @@
-# Resume
-نمونه رزومه مبتنی بر https://github.com/posquit0/Awesome-CV
--- delBikhood.bat
+++
@@ -1,3 +0,0 @@
-del /S *.aux,*.blg,*.brf,*.idx,*.ilg,*.ind,*.loa,*.lof,*.log,*.lot,*.out,*.pdfsync
-del /S *.ptd,*.synctex,*.toc,*.upa,*.upb,*.asv,*.o,*.upa,*.upb, *.snm, *.bcf,*.gz
-del /S *conflicted*.*, *.~*,*.gnuplot,*.nav,*.tdo,*.glg,*.glo,*.gls,*.los,*.nlo,*.xdy,*.tds,*.obj
